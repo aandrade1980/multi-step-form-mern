b09e3f3b68a4dfbd9f1263f3dd2ecc5fab89df29
Alvaro <varito1@gmail.com>
2021-05-09 00:30:01 -0300

    Updated index.js

--- server/index.js
+++ server/index.js
@@ -1,22 +1,21 @@
+const express = require('express');
 const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../.env.local') });
-const express = require('express');
+const cors = require('cors');
 const userRouter = require('./routers/user');
 require('./db');
 
 const app = express();
-const PORT = process.env.PORT || 3030;
 
-app.use(express.static(path.join(__dirname, '..', 'build')));
+app.use(cors());
+app.use(express.static(path.join(__dirname, '../build')));
 app.use(express.json());
 app.use(userRouter);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '..', 'build')));
-}
-
 app.use('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'))
+  res.sendFile(path.join(__dirname, '../build', 'index.html'))
 );
+
+const PORT = process.env.PORT || 3030;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));

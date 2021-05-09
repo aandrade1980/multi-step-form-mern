e90b72dd59d779cc9969f8e49dc9eb2e8c64c153
Alvaro <varito1@gmail.com>
2021-05-08 23:27:08 -0300

    Updated db.js

--- server/db.js
+++ server/db.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const MONGO_URL =
-  process.env.MONGO_URL ||
   'mongodb+srv://admin:Password.123@cluster0.pfc6u.mongodb.net/form-user?retryWrites=true&w=majority';
 
 mongoose.connect(MONGO_URL, {

6ba2fec8e49485f51aa7b87d08f07a5ba4efb63e
Alvaro <varito1@gmail.com>
2021-05-08 22:20:33 -0300

    Modified server index js

--- server/index.js
+++ server/index.js
@@ -13,8 +13,6 @@ app.use(userRouter);
 
 app.get('/', (req, res) => res.send('<h2>This is from index.js</h2>'));
 
-app.use((req, res, next) =>
-  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'))
-);
+app.use('*', express.static(path.join(__dirname, '..', 'build', 'index.html')));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
